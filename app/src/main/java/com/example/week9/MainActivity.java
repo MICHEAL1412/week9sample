b24508ccc3766cc9cecd671571b7ed312091cce9
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-08-11 12:54:34 +0800

    commit by ng

--- app/src/main/java/com/example/week9/MainActivity.java
+++ app/src/main/java/com/example/week9/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
--- app/src/main/java/com/example/week9/WordRoomDatabase.java
+++ app/src/main/java/com/example/week9/WordRoomDatabase.java
@@ -2,6 +2,7 @@ package com.example.week9;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -13,34 +14,60 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public abstract class WordRoomDatabase extends RoomDatabase {
 
-        public abstract WordDao wordDao();
-        private static WordRoomDatabase INSTANCE;
+    public abstract WordDao wordDao();
 
-        static WordRoomDatabase getDatabase(final Context context) {
-            if (INSTANCE == null) {
-                synchronized (WordRoomDatabase.class) {
-                    if (INSTANCE == null) {
-                        INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                                WordRoomDatabase.class, "word_database")
-                                // Wipes and rebuilds instead of migrating
-                                // if no Migration object.
-                                // Migration is not part of this practical.
-                                .fallbackToDestructiveMigration()
-                                .build();
-                    }
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating
+                            // if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .build();
                 }
             }
-            return INSTANCE;
         }
-        private static RoomDatabase.Callback sRoomDatabaseCallback =
-            new RoomDatabase.Callback(){
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
 
                 @Override
-                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
                     super.onOpen(db);
                     new PopulateDbAsync(INSTANCE).execute();
                 }
             };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
+   }
 }
 
 
